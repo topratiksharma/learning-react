cc0dc2d48770a095df96e1f44b14e751c8e91569
Pratik Sharma <pratiksharma@live.in>
2021-08-31 23:00:31 +0530

    initial commit

---
+++ react-app/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import reportWebVitals from './reportWebVitals';
+
+const element = <h1>Hello world</h1>
+
+ReactDOM.render(element, document.getElementById('root'));
+
+reportWebVitals();
